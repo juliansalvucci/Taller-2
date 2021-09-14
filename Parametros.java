088868d23a3acdf32151c18762576b5900ef8f33
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-14 15:50:38 -0300

    parámetros

---
+++ Parametros.java
@@ -0,0 +1,12 @@
+//Enviar parámetros para comenzar la ejecución.
+
+public class Parametros {
+    public static void main(String[] parametros){
+        if (parametros[0].equals("ENGLISH")) {
+            System.out.println("Hello world");
+        }
+        if (parametros[1].equals("ESPANOL")) {
+            System.out.println("Hola Mundo");
+        }
+    }
+}
